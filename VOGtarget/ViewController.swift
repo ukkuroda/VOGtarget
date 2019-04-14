21051cb5215bfddf0ff4660d0f4da38d1ba7a44f
黒田建彰 <tatsuaki@orange.ocn.ne.jp>
2019-04-14 14:05:50 +0900

    Initial Commit

---
+++ VOGtarget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VOGtarget
+//
+//  Created by kuroda tatsuaki on 2019/04/14.
+//  Copyright © 2019 tatsuaki.kuroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
